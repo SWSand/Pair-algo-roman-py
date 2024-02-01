c37067ccba909a6085de4593e15fbd9e2a560391
Jose Sandoval <Software.Sandoval@gmail.com>
2024-02-01 17:23:20 -0600

    solved

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,4 +1,4 @@
-def to_roman(num):
+def to_roman_lazy(num):
     roman_numerals = {
     'M' : 1000,
     'D' : 500,
@@ -8,13 +8,41 @@ def to_roman(num):
     'V' : 5,
     'I' : 1,
     }
-
     # for loop through dictionary values and compare to num
     ans = ""
+
     for x in roman_numerals :
         if num >= roman_numerals[x] :
             ans += x * (num//roman_numerals[x])
             num = (num % roman_numerals[x])
     return ans
 
-print(to_roman(6))
+def to_roman(num):
+    roman_numerals = {
+    'M' : 1000,
+    'CM': 900,
+    'D' : 500,
+    'CD' : 400,
+    'C' : 100,
+    'XC' : 90,
+    'L' : 50,
+    'XL' : 40,
+    'X' : 10,
+    'IX' : 9,
+    'V' : 5,
+    'IV': 4,
+    'I' : 1,
+    }
+    # for loop through dictionary values and compare to num
+    ans = ""
+
+    for x in roman_numerals :
+        if num >= roman_numerals[x] :
+            ans += x * (num//roman_numerals[x])
+            num = (num % roman_numerals[x])
+    return ans
+
+# print(to_roman_lazy(909))
+# print(to_roman(909))
+
+
